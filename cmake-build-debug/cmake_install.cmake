7c234d2347ed4490564600a6dce00420ef3359ad
Narek Hovhannisyan <nhovhannisya@vmware.com>
2018-11-20 22:21:56 +0400

    fixup

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/nhovhannisya/Desktop/Classes/Interpreter_coursework/Virtual_Machine
+# Install script for directory: /cygdrive/c/Users/mmamyan/CLionProjects/Compiler_Tigran
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/nhovhannisya/Desktop/Classes/Interpreter_coursework/Virtual_Machine/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/cygdrive/c/Users/mmamyan/CLionProjects/Compiler_Tigran/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
